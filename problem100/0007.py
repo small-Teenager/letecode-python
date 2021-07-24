ba2e7f7da8bb8a676499efd395d98266c5113477
zhangyaodong <yaodong199@icloud.com>
2021-07-24 12:41:34 +0800

    add

---
+++ problem100/0007.py
@@ -0,0 +1,33 @@
+'''
+
+给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+
+如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+
+假设环境不允许存储 64 位整数（有符号或无符号）。
+'''
+
+
+class Solution:
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        str_x = str(x)
+        x = ''
+        if str_x[0] == '-':
+            x += '-'
+        x += str_x[len(str_x) - 1::-1].lstrip("0").rstrip("-")
+        x = int(x)
+        if -2 ** 31 < x < 2 ** 31 - 1:
+            return x
+        return 0
+
+
+reverse = Solution()
+res = reverse.reverse(-123)
+print(res)
