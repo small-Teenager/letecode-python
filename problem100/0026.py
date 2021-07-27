3faf893fe0596d2f336c96c49901cd1da9079e7a
Yaodong Zhang <yaodong199@icloud.com>
2021-07-27 21:55:01 +0800

    26

--- problem100/0026.py
+++ problem100/0026.py
@@ -27,6 +27,24 @@ class Solution(object):
                 j += 1
         return i + 1
 
+    def removeDuplicates2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        n = len(nums)
+        fast = slow = 1
+        while fast < n:
+            if nums[fast] != nums[fast - 1]:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
+
 
 a = Solution().removeDuplicates([1, 2, 3, 3])
+print(a)
+b = Solution().removeDuplicates2([1, 2, 3, 3])
 print(a)
