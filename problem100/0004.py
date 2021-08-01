4f40fa914adb971180387f6d9a0e1e8e90daa83d
Yaodong Zhang <yaodong199@icloud.com>
2021-08-01 13:28:05 +0800

    0004

---
+++ problem100/0004.py
@@ -0,0 +1,43 @@
+'''
+4. 寻找两个正序数组的中位数
+给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+'''
+
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        n1 = len(nums1)
+        n2 = len(nums2)
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        k = (n1 + n2 + 1) // 2
+        left = 0
+        right = n1
+        while left < right:
+            m1 = left + (right - left) // 2
+            m2 = k - m1
+            if nums1[m1] < nums2[m2 - 1]:
+                left = m1 + 1
+            else:
+                right = m1
+        m1 = left
+        m2 = k - m1
+        c1 = max(nums1[m1 - 1] if m1 > 0 else float("-inf"), nums2[m2 - 1] if m2 > 0 else float("-inf"))
+        if (n1 + n2) % 2 == 1:
+            return c1
+        c2 = min(nums1[m1] if m1 < n1 else float("inf"), nums2[m2] if m2 < n2 else float("inf"))
+        return (c1 + c2) / 2
+
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+a = Solution().findMedianSortedArrays(nums1, nums2)
+print(a)
