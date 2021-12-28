b273676dda104859c6fad81940cfbb181a73e6eb
yaodong <yaodong199@icloud.com>
2021-12-28 14:47:35 +0800

    3. 无重复字符的最长子串

---
+++ problem100/0003.py
@@ -0,0 +1,36 @@
+'''
+3. 无重复字符的最长子串
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+'''
+
+
+# 滑动窗口
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s: return 0
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len: max_len = cur_len
+            lookup.add(s[i])
+        return max_len
+
+
+s = "abcabcbb"
+
+print(Solution().lengthOfLongestSubstring(s))
